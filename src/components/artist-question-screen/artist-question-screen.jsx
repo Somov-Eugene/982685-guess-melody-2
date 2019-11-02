5f7443958e444c62824b0d307a394cd57e8f536a
Somov Eugene <somov.eugene.2012@gmail.com>
2019-11-02 13:21:14 +0300

    подключен компонент «Аудиоплеер» к компоненту «Угадай исполнителя»

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,89 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ArtistQuestionScreen = (props) => {
-  const {screenIndex, question, onAnswer} = props;
-  const {answers} = question;
+import AudioPlayer from '../audio-player/audio-player.jsx';
+class ArtistQuestionScreen extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-  // style="filter: url(#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"
-  const timerStyle = {
-    filter: `url(#blur)`,
-    transform: `rotate(-90deg) scaleY(-1)`,
-    transformOrigin: `center`
-  };
+    this.state = {
+      isPlaying: false,
+    };
+  }
 
-  return (
-    <section className="game game--artist">
-      <header className="game__header">
-        <a className="game__back" href="#">
-          <span className="visually-hidden">Сыграть ещё раз</span>
-          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-        </a>
+  render() {
+    const {question, onAnswer} = this.props;
+    const {isPlaying} = this.state;
+    const {answers, song} = question;
 
-        <svg className="timer" viewBox="0 0 780 780" xmlns="http://www.w3.org/2000/svg">
-          <circle className="timer__line" cx="390" cy="390" r="370" style={timerStyle} />
-        </svg>
-
-        <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
-          <span className="timer__mins">05</span>
-          <span className="timer__dots">:</span>
-          <span className="timer__secs">00</span>
-        </div>
-
-        <div className="game__mistakes">
-          <div className="wrong"></div>
-          <div className="wrong"></div>
-          <div className="wrong"></div>
-        </div>
-      </header>
-
+    return (
       <section className="game__screen">
         <h2 className="game__title">Кто исполняет эту песню?</h2>
         <div className="game__track">
-          <div className="track">
-            <button className="track__button track__button--play" type="button"></button>
-            <div className="track__status">
-              <audio></audio>
-            </div>
-          </div>
+          <AudioPlayer
+            isPlaying={isPlaying}
+            onPlayButtonClick={() => this.setState({isPlaying: !isPlaying})}
+            src={song.src}
+          />
         </div>
 
         <form className="game__artist" onChange={onAnswer}>
-          {
-            answers.map((it, i) => {
-              return (
-                <div className="artist" key={`${screenIndex}-answer-${i}`}>
-                  <input className="artist__input visually-hidden" type="radio" name="answer" id={`answer-${i}`} value={it.artist} />
-                  <label className="artist__name" htmlFor={`answer-${i}`}>
-                    <img className="artist__picture" src={it.picture} alt={it.artist} />
-                    {it.artist}
-                  </label>
-                </div>
-              );
-            })
-          }
+          {answers.map((it, i) => <div className="artist" key={i}>
+            <input className="artist__input visually-hidden" type="radio" name="answer" id={`artist-${i}`} value={it.artist} />
+            <label className="artist__name" htmlFor={`artist-${i}`}>
+              <img className="artist__picture" src={it.picture} alt={it.artist} />
+              {it.artist}
+            </label>
+          </div>)}
         </form>
       </section>
-    </section>
-  );
-};
+    );
+  }
+}
 
 ArtistQuestionScreen.propTypes = {
-  screenIndex: PropTypes.number.isRequired,
+  onAnswer: PropTypes.func.isRequired,
   question: PropTypes.shape({
-    type: PropTypes.oneOf([`artist`]).isRequired,
+    answers: PropTypes.arrayOf(PropTypes.shape({
+      artist: PropTypes.string.isRequired,
+      picture: PropTypes.string.isRequired,
+    })).isRequired,
     song: PropTypes.shape({
       artist: PropTypes.string.isRequired,
-      src: PropTypes.string.isRequired
+      src: PropTypes.string.isRequired,
     }).isRequired,
-    answers: PropTypes.arrayOf(
-        PropTypes.shape({
-          picture: PropTypes.string.isRequired,
-          artist: PropTypes.string.isRequired
-        }).isRequired
-    ).isRequired
+    type: PropTypes.oneOf([`genre`, `artist`]).isRequired,
   }).isRequired,
-  onAnswer: PropTypes.func.isRequired
 };
 
 export default ArtistQuestionScreen;
