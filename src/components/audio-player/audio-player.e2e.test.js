c403d3528086d5b11404d2b612a5fafee9af096a
Somov Eugene <somov.eugene.2012@gmail.com>
2019-11-02 20:53:08 +0300

    исправления e2e-теста

--- src/components/audio-player/audio-player.e2e.test.js
+++ src/components/audio-player/audio-player.e2e.test.js
@@ -16,6 +16,11 @@ describe(`Проверка <AudioPlayer>`, () => {
   let audioPlayer = null;
   let btn = null;
 
+  window.HTMLMediaElement.prototype.load = () => { /* do nothing */ };
+  window.HTMLMediaElement.prototype.play = () => { /* do nothing */ };
+  window.HTMLMediaElement.prototype.pause = () => { /* do nothing */ };
+  window.HTMLMediaElement.prototype.addTextTrack = () => { /* do nothing */ };
+
   beforeEach(() => {
     audioPlayer = mount(<AudioPlayer
       src = {src}
@@ -27,19 +32,20 @@ describe(`Проверка <AudioPlayer>`, () => {
   });
 
   it(`начальное состояние компонента: аудиофайл не проигрывается`, () => {
+    audioPlayer.setState({isLoading: false});
     expect(audioPlayer.state(`isPlaying`)).toBe(false);
   });
 
   it(`при нажатии на кнопку «Воспроизведение» у компонента изменится состояние на «воспроизведение»`, () => {
     audioPlayer.update();
-    btn.simulate(`click`);
+    btn.at(0).simulate(`click`);
     audioPlayer.setState({isPlaying: true});
     expect(audioPlayer.state(`isPlaying`)).toBe(true);
   });
 
   it(`повторный клик изменит состояние на «пауза»`, () => {
     audioPlayer.update();
-    btn.simulate(`click`);
+    btn.at(0).simulate(`click`);
     audioPlayer.setState({isPlaying: false});
     expect(audioPlayer.state(`isPlaying`)).toBe(false);
   });
