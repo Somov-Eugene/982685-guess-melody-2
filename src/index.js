1f23c1c167a588d1ca65d4655cb4520c45489ba0
Somov Eugene <somov.eugene.2012@gmail.com>
2019-11-02 12:54:44 +0300

    приведены названия props в соответствие с демо

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
-import settings from './mocks/settings';
+import gameSettings from './mocks/settings';
 import questions from './mocks/questions';
 
 const init = (gameQuestions) => {
+  const {errorCount, gameTime} = gameSettings;
+
   ReactDOM.render(
       <App
-        gameTime = {settings.gameTime}
-        errorCount = {settings.errorCount}
+        errorCount = {errorCount}
+        gameTime = {gameTime}
         questions = {gameQuestions}
       />,
       document.querySelector(`#root`)
