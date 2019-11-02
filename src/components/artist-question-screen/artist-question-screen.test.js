32cb6fc9c330be7463367d31fac4a8a1b4f11b5c
Somov Eugene <somov.eugene.2012@gmail.com>
2019-11-02 19:28:00 +0300

    обновлен снеп-шот тест

--- src/components/artist-question-screen/artist-question-screen.test.js
+++ src/components/artist-question-screen/artist-question-screen.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 import questions from '../../mocks/questions';
+import createNodeMock from '../../mocks/test-utility';
 import ArtistQuestionScreen from './artist-question-screen';
 
 describe(`Проверка <ArtistQuestionScreen>`, () => {
@@ -9,12 +10,13 @@ describe(`Проверка <ArtistQuestionScreen>`, () => {
     const question = 1;
     const currentQuestion = questions[question];
     const handleAnswer = jest.fn();
+    const options = {createNodeMock};
 
     const tree = renderer
       .create(<ArtistQuestionScreen
         question = {currentQuestion}
         onAnswer = {handleAnswer}
-      />)
+      />, options)
       .toJSON();
 
     expect(tree).toMatchSnapshot();
