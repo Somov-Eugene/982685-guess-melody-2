a5b366efaad0b5d59ec9c325e3d0769c73432bc3
Somov Eugene <somov.eugene.2012@gmail.com>
2019-11-02 13:29:13 +0300

    изменены props при вызове компонента

--- src/components/artist-question-screen/artist-question-screen.test.js
+++ src/components/artist-question-screen/artist-question-screen.test.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import ArtistQuestionScreen from './artist-question-screen';
 import questions from '../../mocks/questions';
+import ArtistQuestionScreen from './artist-question-screen';
 
 describe(`Проверка <ArtistQuestionScreen>`, () => {
   it(`компонент <ArtistQuestionScreen> корректно отрисован`, () => {
     const question = 1;
     const currentQuestion = questions[question];
-    const handlerClick = jest.fn();
+    const handleAnswer = jest.fn();
 
     const tree = renderer
       .create(<ArtistQuestionScreen
-        screenIndex = {question}
         question = {currentQuestion}
-        onAnswer = {handlerClick}
+        onAnswer = {handleAnswer}
       />)
       .toJSON();
 
