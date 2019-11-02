1fd9d4c5bfdd84a8bc6fde92c9530268a0f74404
Somov Eugene <somov.eugene.2012@gmail.com>
2019-11-02 19:27:21 +0300

    обновлен снеп-шот тест

--- src/components/genre-question-screen/genre-question-screen.test.js
+++ src/components/genre-question-screen/genre-question-screen.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 import questions from '../../mocks/questions';
+import createNodeMock from '../../mocks/test-utility';
 import GenreQuestionScreen from './genre-question-screen';
 
 describe(`Проверка <GenreQuestionScreen>`, () => {
@@ -9,12 +10,13 @@ describe(`Проверка <GenreQuestionScreen>`, () => {
     const question = 0;
     const currentQuestion = questions[question];
     const handleAnswer = jest.fn();
+    const options = {createNodeMock};
 
     const tree = renderer
       .create(<GenreQuestionScreen
         question = {currentQuestion}
         onAnswer = {handleAnswer}
-      />)
+      />, options)
       .toJSON();
 
     expect(tree).toMatchSnapshot();
